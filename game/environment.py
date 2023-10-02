ffb8424f9feff10e0e0ab42f6f68abbc09c89dec
cartgr <carterblair@uvic.ca>
2023-10-02 19:41:07 -0400

    multi agent support

--- game/environment.py
+++ game/environment.py
@@ -33,7 +33,7 @@ class Orchard:
 
 
 class GameEnvironment:
-    def __init__(self):
+    def __init__(self, num_agents=2):
         self.river = River()
         self.orchard = Orchard()
         self.board = [[" " for _ in range(8)] for _ in range(8)]
@@ -42,45 +42,75 @@ class GameEnvironment:
             self.board[0][i] = "T"  # Orchard trees
             self.board[7][i] = "R"  # River
 
-        # Start the agent in the middle of the board
-        self.agent_pos = [4, 4]
-        self.board[4][4] = "A"
-        self.agent_reward = 0
+        self.agent_positions = {
+            f"agent_{i+1}": np.array([0, 0]) for i in range(num_agents)
+        }
+        self.agent_rewards = {f"agent_{i+1}": 0 for i in range(num_agents)}
+
+        # initialize the agents on the board
+        for i in range(num_agents):
+            # select a random spot on the board
+            agent_pos = np.random.randint(0, 8, size=2)
+            while self.board[agent_pos[0]][agent_pos[1]] not in [" ", "T", "R"]:
+                agent_pos = np.random.randint(0, 8, size=2)
+            self.board[agent_pos[0]][agent_pos[1]] = f"{i+1}"
+            self.agent_positions[f"agent_{i+1}"] = agent_pos
 
     def _is_valid_move(self, new_pos):
         return 0 <= new_pos[0] < 8 and 0 <= new_pos[1] < 8
 
-    def step(self, agent_action):
-        # Update the agent's position based on the action
-        prev_pos = self.agent_pos.copy()
-        if agent_action == "move north":
-            self.agent_pos[0] -= 1
-        elif agent_action == "move south":
-            self.agent_pos[0] += 1
-        elif agent_action == "move east":
-            self.agent_pos[1] += 1
-        elif agent_action == "move west":
-            self.agent_pos[1] -= 1
-        elif agent_action == "clean river" and self.agent_pos[0] == 7:
-            self.river.clean()
-        elif agent_action == "pick fruit" and self.agent_pos[0] == 0:
-            picked_fruit = (
-                self.orchard.pick_fruit()
-            )  # 0 if no fruit picked, 1 if fruit picked
-            if picked_fruit == 1:
-                self.agent_reward += 1
+    def step(self, agent_actions: dict = {}):
+        # agent_actions is a dictionary of actions for each agent
+        # agent_actions = {"agent_1": "move north", "agent_2": "clean river"}
 
-        # Ensure that the agent's move is within the bounds of the board
-        if self._is_valid_move(self.agent_pos):
-            self.board[prev_pos[0]][prev_pos[1]] = (
-                " "
-                if prev_pos[0] != 0 and prev_pos[0] != 7
-                else self.board[prev_pos[0]][prev_pos[1]]
-            )
-            self.board[self.agent_pos[0]][self.agent_pos[1]] = "A"
-        else:
-            # Revert to previous position if the move isn't valid
-            self.agent_pos = prev_pos
+        default_tile = [[" " for _ in range(8)] for _ in range(8)]
+        default_tile[0] = ["T" for _ in range(8)]
+        default_tile[7] = ["R" for _ in range(8)]
+
+        # First, let's move the agents
+
+        for agent, action in agent_actions.items():
+            if action == "move north":
+                new_pos = self.agent_positions[agent] + np.array([-1, 0])
+            elif action == "move south":
+                new_pos = self.agent_positions[agent] + np.array([1, 0])
+            elif action == "move west":
+                new_pos = self.agent_positions[agent] + np.array([0, -1])
+            elif action == "move east":
+                new_pos = self.agent_positions[agent] + np.array([0, 1])
+            else:
+                new_pos = self.agent_positions[agent]
+
+            if self._is_valid_move(new_pos):
+                # change the tile back to the default tile
+                self.board[self.agent_positions[agent][0]][
+                    self.agent_positions[agent][1]
+                ] = default_tile[self.agent_positions[agent][0]][
+                    self.agent_positions[agent][1]
+                ]
+                # update the agent position on the board with the agent number
+                # just extract the number from the agent name
+                self.board[new_pos[0]][new_pos[1]] = agent.split("_")[1]
+                # update the agent position in the agent_positions dictionary
+                self.agent_positions[agent] = new_pos
+
+            # Now let's do the actions
+            if action == "clean river":
+                self.river.clean()
+            elif action == "pick fruit":
+                self.agent_rewards[agent] += self.orchard.pick_fruit()
+
+        # Now let's update the board
+        # First, let's reset the board
+        self.board = [[" " for _ in range(8)] for _ in range(8)]
+        # Let's initialize the river and orchard on the board
+        for i in range(8):
+            self.board[7][i] = "R"
+            self.board[0][i] = "T"
+
+        # Now let's update the board with the agent positions
+        for agent, pos in self.agent_positions.items():
+            self.board[pos[0]][pos[1]] = agent.split("_")[1]
 
         self.orchard.grow_fruits(self.river.dirtiness)
         # make river 1 unit dirtier with 0.2 probability
@@ -88,15 +118,22 @@ class GameEnvironment:
             self.river.dirtiness += 1
 
     def display(self):
-        for row in self.board:
-            print(" ".join(row))
-        print("\n")
+        # display the board with numbers on the rows and columns
+        print("  ", end="")
+        for i in range(8):
+            print(i, end=" ")
+        print()
+        for i in range(8):
+            print(i, end=" ")
+            for j in range(8):
+                print(self.board[i][j], end=" ")
+            print()
 
     def get_state(self):
         return {
-            "agent_pos": self.agent_pos,
+            "agent_positions": self.agent_positions,
             "river_dirtiness": self.river.dirtiness,
             "orchard_fruits": self.orchard.fruits,
-            "agent_reward": self.agent_reward,
+            "agent_rewards": self.agent_rewards,
             "board": self.board,
         }
--- main.py
+++ main.py
@@ -3,47 +3,83 @@ from agents.gpt4_agent import GPT4Agent
 import pandas as pd
 import datetime
 
-NUMBER_OF_STEPS = 100
-API_KEY = "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"  # Remember to set your API key here!
+NUMBER_OF_STEPS = 200
+API_KEY = "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"
 
 
-def main():
-    env = GameEnvironment()
-    agent = GPT4Agent(API_KEY)
+def main(num_agents=2):
+    env = GameEnvironment(num_agents=num_agents)
+
+    agent_positions = env.agent_positions
+
+    agents = {}
+
+    for agent, pos in agent_positions.items():
+        if agent == "agent_1":
+            agents[agent] = GPT4Agent(
+                agent_name=agent, api_key=API_KEY, pos=pos, agent_type="cooperative"
+            )
+        else:
+            agents[agent] = GPT4Agent(
+                agent_name=agent, api_key=API_KEY, pos=pos, agent_type="cooperative"
+            )
 
     # For logging data
     log_data = {
         "step": [],
-        "agent_action": [],
+        "agent_actions": {f"agent_{i+1}": [] for i in range(num_agents)},
         "river_dirtiness": [],
         "orchard_fruits": [],
-        "agent_reward": [],
+        "agent_rewards": {f"agent_{i+1}": [] for i in range(num_agents)},
     }
 
     for step in range(NUMBER_OF_STEPS):
         game_state = env.get_state()
-        agent_action = agent.make_decision(game_state)
-        env.step(agent_action)
+        agent_actions = {}
+        for agent in agents.values():
+            agent_action = agent.make_decision(
+                game_state
+            )  # need to make a loop or something
+            agent_actions[agent.agent_name] = agent_action
+
+            log_data["agent_actions"][agent.agent_name].append(agent_action)
+
+        env.step(agent_actions)  # need to pass a dictionary of actions
 
         # Log data for analysis
         log_data["step"].append(step)
-        log_data["agent_action"].append(agent_action)
-        log_data["river_dirtiness"].append(game_state["river_dirtiness"])
-        log_data["orchard_fruits"].append(game_state["orchard_fruits"])
-        log_data["agent_reward"].append(game_state["agent_reward"])
+        log_data["river_dirtiness"].append(env.river.dirtiness)
+        log_data["orchard_fruits"].append(env.orchard.fruits)
+        for agent_name in env.agent_rewards.keys():
+            log_data["agent_rewards"][agent_name].append(env.agent_rewards[agent_name])
 
         # Optional: Display the game environment at each step
         print(f"Step: {step}")
-        print(f"Agent Action: {agent_action}")
         env.display()
+        print(f"Agent actions: {agent_actions}")
         print("\n\n")
 
     # Now we will log the data to a CSV file
+    # some of the data is nested so we need to flatten it
+    # first we will flatten the agent_actions
+    for agent, action in log_data["agent_actions"].items():
+        log_data[f"{agent}_action"] = action
+    # now we will flatten the agent_rewards
+    for agent, reward in log_data["agent_rewards"].items():
+        log_data[f"{agent}_reward"] = reward
 
-    df = pd.DataFrame(log_data)
-    # log file name should be date and time
-    log_file_name = "log_" + str(datetime.datetime.now()) + ".csv"
-    df.to_csv("game_logs/" + log_file_name, index=False)
+    # now we will drop the original agent_actions and agent_rewards
+    log_data.pop("agent_actions")
+    log_data.pop("agent_rewards")
+
+    # now we will convert the log_data dictionary to a pandas dataframe
+    log_df = pd.DataFrame(log_data)
+
+    # now we will save the dataframe to a csv file
+    # we will use the current date and time to name the file
+    now = datetime.datetime.now()
+    date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+    log_df.to_csv(f"game_logs/log_{date_time}.csv")
 
 
 if __name__ == "__main__":
